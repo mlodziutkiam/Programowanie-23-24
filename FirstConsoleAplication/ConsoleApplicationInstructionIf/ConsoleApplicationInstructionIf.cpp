8300297f9759c09f15b09e6624e2fc95bc80ac00
Mlodziutki <joemamma@kys.com>
2023-09-29 13:15:39 +0200

    qsasdxffaser

---
+++ FirstConsoleAplication/ConsoleApplicationInstructionIf/ConsoleApplicationInstructionIf.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+
+//Napisz funkcjê która podzieli dwie liczby 
+
+void task1()
+{
+    double firstNumber, secondNumber;
+    cout << "Podaj pierwsz¹ liczbe: ";
+    cin >> firstNumber;
+    cout << "Podaj drug¹ liczbe: ";
+    cin >> secondNumber;
+
+    double quotient = firstNumber / secondNumber;
+
+    cout << "Wynik dzielenia wynosi: " << quotient;
+    
+}
+
+void task2()
+{
+        
+}
+int main()
+{
+    setlocale(LC_CTYPE, "polish");
+    task2();
+}
+
