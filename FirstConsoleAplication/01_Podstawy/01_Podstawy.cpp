05dbb7c15db734e2294ce478b9b4c4cbd3058f48
Mlodziutki <joemamma@kys.com>
2024-03-15 13:17:17 +0100

    commitniga

--- FirstConsoleAplication/01_Podstawy/01_Podstawy.cpp
+++ FirstConsoleAplication/01_Podstawy/01_Podstawy.cpp
@@ -12,27 +12,87 @@ Napisz program który zaprezentuje mo¿liwoœci obiektu na podstawie tej klasy.
 */
 
 class Rectangle {
-private:
-	float lenght;
+	float length;
 	float height;
 
 public:
 	Rectangle(float l=1, float h=3)
 	{
-		l = lenght;
+		l = length;
 		h = height;
 	}
 
 	float calculatePerimeter() {
-		return lenght * 2 + height * 2;
+		return length * 2 + height * 2;
 	}
 	float calculateArea() {
-		return lenght * height;
+		return length * height;
 	}
 	float rectangleInfo() {
 		cout << "Obwód:" << calculatePerimeter() << "\n";
 		cout << "Pole: " << calculateArea() << "\n";
-		cout << "D³ugoœæ: " << lenght << "\n";
+		cout << "D³ugoœæ: " << length << "\n";
 		cout << "Wysokoœæ: " << height << "\n";
 	}
 };
+
+class BankAccount {
+	float balance = 1000;
+
+public:
+	void showBalance() {
+		cout<< "Account balance: "  << balance;
+	}
+	void depositMoney() {
+		double deposit;
+		cout << "How much money would you like to deposit?\n";
+		cin >> deposit;
+		balance += deposit;
+		cout << "New account balance: " << balance;
+}
+	void withdrawMoney() {
+		double withdraw;
+		while(true){
+			cout << "How much money would you like to withdraw?\n";
+			cin >> withdraw;
+			if (withdraw > balance) {
+				cout << "Not enough funds!\n";
+				continue;
+			}
+			else {
+				balance -= withdraw;
+				cout << "Withdrawal succeed. New Account balance: " << balance;
+				break;
+			}
+		}
+	}
+	void transferMoney() {
+
+	}
+	
+
+
+	
+};
+
+
+int main() {
+	BankAccount firstAccount;
+	
+	firstAccount.withdrawMoney();
+
+	
+}
+
+
+/*
+ZADANIE
+Zdefiniuj klase opisuj¹ce konto bankowe. Mo¿liwoœci klasy:
+* konstruktory
+* metoda która dokona wp³aty na konto
+* metoda która wyp³aty z konta
+* metoda która wykona przelew na inne konto
+* metodê która poka¿e informacje o koncie.
+
+Napisz program który zaprezentuje mo¿liwoœci obiektu na podstawie tej klasy.
+*/
